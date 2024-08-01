1f946c22c76a0156305b3ae34a0a34d011f91fed
Shrutikumari1707 <cse.23bcsd28@silicon.ac.in>
2024-08-01 20:22:16 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ import './App.css';
 
 const App = () => {
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/user/home" element={<UserHome />} />
